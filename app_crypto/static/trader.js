552ed1c1a20be0e9615ff9a64abcf22267a1a1d3
Gabriela M. Fernández <galalou2010@gmail.com>
2023-01-31 23:19:49 +0100

    header, footer, tabla y algunos estilos

---
+++ app_crypto/static/trader.js
@@ -0,0 +1,18 @@
+function addTableRow() {
+  var table = document.getElementById("trading-table");
+  var row = table.insertRow();
+
+  var cell1 = row.insertCell();
+  var cell2 = row.insertCell();
+  var cell3 = row.insertCell();
+  var cell4 = row.insertCell();
+  var cell5 = row.insertCell();
+  var cell6 = row.insertCell();
+
+  cell1.innerHTML = "Hello";
+  cell2.innerHTML = "Hello";
+  cell3.innerHTML = "Hello";
+  cell4.innerHTML = "Hello";
+  cell5.innerHTML = "Hello";
+  cell6.innerHTML = "Hello";
+}
